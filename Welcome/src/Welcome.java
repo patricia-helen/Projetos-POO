d430e310146be3dc24ba2a16413692c445c2ee04
Patricia Moreira <patricia.helen@ufvjm.edu.br>
2024-03-26 16:46:31 -0300

    Exemplo do Livro

---
+++ Welcome/src/Welcome.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author pat
+ */
+// Figura 2.1: Welcome.java
+// Programa de impressão de texto.
+
+public class Welcome {
+    //o método main  inicia a execução do aplicativo java
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Programming!");
+    }//fim do método main
+    
+}//fim da classe Welcome
